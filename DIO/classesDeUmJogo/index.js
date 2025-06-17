be1975353ad6975941abd861de2c4fb4631ac15f
Felipe Andrade <felipevsandrade@gmail.com>
2025-06-17 20:07:35 -0300

    first commit

---
+++ DIO/classesDeUmJogo/index.js
@@ -0,0 +1,30 @@
+// Exemplo de código para uma classe de um jogo
+// Definindo a classe Heroi
+class Heroi {
+    constructor(nome, idade, tipo) {
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;;
+    }
+
+    atacar() {
+        let ataque = "";
+        switch (this.tipo) {
+            case "guerreiro":
+                ataque = "usou espada";
+                break;
+            case "mago":
+                ataque = "usou magia";
+                break;
+            case "monge":
+                ataque = "usou artes marciais";
+                break;
+            case "ninja":
+                ataque = "usou shurikens";
+                break;
+            default:
+                ataque = "atacou com arma desconhecida";
+        }
+        console.log(`O herói ${this.tipo} atacou usando ${ataque}!`);
+    }
+}    
